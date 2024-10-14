bad4066562e26fe4cd7eb836aeb028889a536cf0
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-10-14 15:25:33 +0300

    :star: error handling: adjust the Error Page for custom errors

--- src/pages/[...catchAll].vue
+++ src/pages/[...catchAll].vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import AppErrorPage from '@/components/AppError/AppErrorPage.vue'
 
-useErrorStore().setError()
+useErrorStore().setError({ error: 'Page Not Found', customCode: 404 })
 </script>
 
 <template>
