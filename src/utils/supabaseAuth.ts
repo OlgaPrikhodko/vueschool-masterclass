e4810923090eed65e33359af69c602729ae0785e
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-10-16 13:50:10 +0300

    ♻️ Auth: setUp auth store and integrate it with utility functions

--- src/utils/supabaseAuth.ts
+++ src/utils/supabaseAuth.ts
@@ -2,6 +2,8 @@ import type { LoginForm, RegisterForm } from '@/types/AuthForm'
 
 import { supabase } from '@/lib/supabaseClient'
 
+const authStore = useAuthStore()
+
 export const register = async (formData: RegisterForm) => {
   const { data, error } = await supabase.auth.signUp({
     email: formData.email,
@@ -19,17 +21,21 @@ export const register = async (formData: RegisterForm) => {
 
     if (error) return console.log('Profile error: ', error)
 
+    authStore.setAuth(data.session)
+
     return true
   }
 }
 
 export const login = async (formData: LoginForm) => {
-  const { error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email: formData.email,
     password: formData.password
   })
 
   if (error) return console.log('Login Error: ', error)
+
+  authStore.setAuth(data.session)
 
   return true
 }
