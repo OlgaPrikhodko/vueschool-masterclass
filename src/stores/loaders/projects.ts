ff597ea629bd1877adc224926eb72ba84d28ea86
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-10-31 08:52:18 +0200

    :zap: perf: implement stale-while-revalidate approach for caching data

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -5,6 +5,20 @@ import { useMemoize } from '@vueuse/core'
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
 
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          console.log('Cached and fresh data matched!')
+          return
+        } else {
+          console.log('Somehting has changed!')
+          loadProjects.delete('projects')
+        }
+      })
+    }
+  }
+
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
 
   const getProjects = async () => {
@@ -13,6 +27,8 @@ export const useProjectsStore = defineStore('projects-store', () => {
     if (error) useErrorStore().setError({ error, customCode: status })
 
     projects.value = data
+
+    validateCache()
   }
 
   return {
